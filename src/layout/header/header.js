048260673eece31f6a353e8370e7796312d26be6
10704302 <wallace_h_wu@wistron.com>
2020-04-15 10:53:32 +0800

    update project structure

---
+++ src/layout/header/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>我是 Header</div>
+    );
+};
+
+export default Header
---
+++ src/layout/sidebar/sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+const Sidebar = () => {
+    return (
+        <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/counter">Counter</Link></li>
+            <li><Link to="/user-list">User List</Link></li>
+        </ul>
+    );
+};
+
+export default Sidebar
