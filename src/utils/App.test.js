621f175b21ca512815748ead0174954b8671fbbf
Wallace H Wu <wallace_h_wu@wistron.com>
2020-05-02 19:05:13 +0800

    update project structure

--- src/utils/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Provider from '../containers/Provider';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Provider />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
