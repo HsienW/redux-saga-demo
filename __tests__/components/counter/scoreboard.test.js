7fe726b3f23d865030538c48818a976237c9b3d6
Wallace H Wu <wallace_h_wu@wistron.com>
2020-05-04 19:50:02 +0800

    update unit test case

--- __tests__/components/counter/scoreboard.test.js
+++ __tests__/components/counter/scoreboard.test.js
@@ -2,23 +2,28 @@ import React from 'react';
 import {describe, expect} from '@jest/globals';
 import {render} from '@testing-library/react';
 import {CounterScoreboard} from '../../../src/components';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import toBeInTheDocument from '@testing-library/jest-dom';
+import counterReducer from '../../../src/redux/counter/counter-reducer';
 
-// jest.mock('counterReducer', () => ({
-//     useSelector: jest.fn()
-// }));
+expect.extend({toBeInTheDocument});
 
 describe('CounterScoreboard', () => {
+    const store = createStore(counterReducer);
+    const {component} = render(
+        <Provider store={store}>
+            <CounterScoreboard/>
+        </Provider>
+    );
 
+    //TODO need change other unit test coding for have redux case.
     test('should create', () => {
-        const component = render(<CounterScoreboard/>);
-        expect(component).toBeTruthy();
+        const {component} = render(<CounterScoreboard/>);
+        expect(component('counter-scoreboard')).toBeInTheDocument();
     });
 
-    describe('CounterScoreboard state check', () => {
-        test('counter state value should is 1', async () => {
-            const component = render(<CounterScoreboard/>);
-            component.counter = 1;
-            expect(component.getByTestId('counter-scoreboard').textContent).toBe('1');
-        });
+    test('counter state value should is 1', () => {
+        expect(component('counter-scoreboard').textContent).toBe('1');
     });
 });
