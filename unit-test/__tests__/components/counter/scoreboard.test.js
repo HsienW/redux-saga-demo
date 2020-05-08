7ea7a1a98daf9aa8825369237ca189123e106683
Wallace H Wu <wallace_h_wu@wistron.com>
2020-05-08 11:04:19 +0800

    update scoreboard case & ut-utils

--- unit-test/__tests__/components/counter/scoreboard.test.js
+++ unit-test/__tests__/components/counter/scoreboard.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {describe, expect} from '@jest/globals';
-import {Counter} from '../../../../src/containers';
+// import {Counter} from '../../../../src/containers';
 import {CounterScoreboard} from '../../../../src/components';
-import {fireEvent} from '@testing-library/react';
-import {providerComponentGenerator} from '../../../test-common/test-common';
-import * as reactRedux from 'react-redux';
+// import {fireEvent} from '@testing-library/react';
+import {providerComponentGenerator} from '../../../test-utils/test-utils';
+import counterReducer from '../../../../src/redux/counter/counter-reducer';
+// import * as reactRedux from 'react-redux';
 import '@testing-library/jest-dom';
 
 describe('CounterScoreboard', () => {
@@ -13,20 +14,27 @@ describe('CounterScoreboard', () => {
         expect(getByTestId('counter-scoreboard')).toBeInTheDocument();
     });
 
-    // todo should check 'mockDispatch.mock.calls' redux type whether meet current testing type.
+    // todo should check redux type whether meet current testing type.
     test('CounterScoreboard dispatch add action', () => {
-        const mockCounterValue = {counter: 1};
-        const mockRedux = jest.spyOn(reactRedux, 'useDispatch');
-        // jest.fn 使用上會需要賦予基本的回傳值, 才能夠讓正常執行模擬一個function
-        const mockDispatch = jest.fn();
 
-        // mockReturnValueOnce or mockReturnValue 前者為只回傳一次, 後者為永久回傳該
-        mockRedux.mockReturnValue(mockCounterValue);
+        const {getByText} = providerComponentGenerator(
+            <CounterScoreboard/>,
+            counterReducer,
+            {counter: 10}
+        );
+        expect(getByText('counter-scoreboard')).toHaveTextContent('計分板: 10');
+
+        // const mockCounterValue = {counter: 1};
+        // const mockRedux = jest.spyOn(reactRedux, 'useDispatch');
+        // // jest.fn 使用上會需要賦予基本的回傳值, 才能夠讓正常執行模擬一個function
+        // const mockDispatch = jest.fn();
         //
-        const {getByTestId} = providerComponentGenerator(<Counter/>);
-        const incrementCounterBtn = getByTestId('increment-counter-btn');
-        fireEvent.click(incrementCounterBtn);
-        // console.log(mockDispatch.mock.calls[0][0]);
-        expect(mockDispatch.mock.calls).toEqual({type: 'VALUE_CHANGING_SUCCESS'});
+        // // mockReturnValueOnce or mockReturnValue 前者為只回傳一次, 後者為永久回傳該
+        // mockRedux.mockReturnValue(mockDispatch);
+        // //
+        // const {getByTestId} = providerComponentGenerator(<CounterScoreboard/>);
+        // const incrementCounterBtn = getByTestId('increment-counter-btn');
+        // fireEvent.click(incrementCounterBtn);
+        // expect(getByTestId('counter-scoreboard')).toEqual('計分板: 1');
     });
 });
--- unit-test/test-utils/test-utils.js
+++ unit-test/test-utils/test-utils.js
@@ -2,14 +2,15 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
-import counterReducer from '../../src/redux/counter/counter-reducer';
 
-const rootReducer = combineReducers({
-    counterReducer
-});
+const createRootReducer = (reducer) => {
+    return combineReducers({
+        reducer
+    });
+};
 
-export const providerComponentGenerator = (component, initState) => {
-    const store = createStore(rootReducer, initState);
+export const providerComponentGenerator = (component, reducer, initState) => {
+    const store = createStore(createRootReducer(reducer), initState);
     return render(
         <Provider store={store}>
             {component}
