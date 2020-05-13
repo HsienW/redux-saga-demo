cb7f752ec3cdb76a25fd2e65983e83a97187bc44
Wallace H Wu <wallace_h_wu@wistron.com>
2020-05-13 15:41:52 +0800

    add counter case

--- src/components/counter/scoreboard.js
+++ src/components/counter/scoreboard.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 
-const CounterScoreboard = () => {
+export const CounterScoreboard = () => {
     const counter = useSelector(state => state.counterReducer.counter);
     return (
         <div data-testid='counter-scoreboard'>計分板: {counter}</div>
     );
 };
-
-export default CounterScoreboard;
