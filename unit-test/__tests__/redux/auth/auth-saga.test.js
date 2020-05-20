69c4712d928027b18b5011701637dda4188d2715
Wallace H Wu <wallace_h_wu@wistron.com>
2020-05-20 11:37:11 +0800

    add ut auth saga case

--- unit-test/__tests__/redux/auth/auth-saga.test.js
+++ unit-test/__tests__/redux/auth/auth-saga.test.js
@@ -4,42 +4,57 @@ import {expectSaga} from 'redux-saga-test-plan';
 import {profileApiSimulation, subscribeApiSimulation} from '../../../../src/api/api-simulation';
 import {
     loginSaga,
-    loginSuccess,
-    getSubscribeSuccess,
-    loginAction,
-    subscribeAction
+    // loginSuccess,
+    // getSubscribeSuccess,
+    // loginAction,
+    // subscribeAction
 } from '../../../../src/redux/auth/auth-saga';
-import * as matchers from 'redux-saga-test-plan/matchers';
+// import * as matchers from 'redux-saga-test-plan/matchers';
 
 describe('AuthSaga', () => {
-    test('API: Get Login Data', () => {
-        const testData = {
-            account: 'test',
-            password: 'test'
-        };
-        const apiGenerator = loginSaga(testData.account, testData.password);
-        const callGetDataApi = apiGenerator.next().value;
-        console.log(callGetDataApi);
-        // expect(callGetDataApi).toEqual(call(profileApiSimulation));
-    });
+    // test('API: Get Login Data', () => {
+    //     // const testData = {
+    //     //     account: 'test',
+    //     //     password: 'test'
+    //     // };
+    //     // const apiGenerator = loginSaga(testData.account, testData.password);
+    //     // const callGetDataApi = apiGenerator.next().value;
+    //     // console.log(callGetDataApi);
+    //     // expect(callGetDataApi).toEqual(call(profileApiSimulation));
+    // });
 
     // todo need change to better test method for saga function.
     test('provides values for effects inside arrays', () => {
-        const testData = {
-            account: 'test',
-            password: 'test'
+        const mockProfileRespond = {
+            userName: 'demo',
+            token: '1234567890'
+        };
+
+        const mockSubscribeRespond = {
+            title: 'Subscribe',
+            subscribeItem: [
+                {
+                    title: 'Subscribe A',
+                    number: 1
+                },
+            ]
         };
         return expectSaga(loginSaga)
+            // .provide({
+            //     all: () => [mockProfileRespond, mockSubscribeRespond],
+            // })
             .provide([
-                [matchers.call.fn(profileApiSimulation), testData.account, testData.password],
-                [matchers.call.fn(subscribeApiSimulation), testData.account, testData.password],
+                // call(profileApiSimulation,'123', '456'),
+                // call(subscribeApiSimulation,'789', '000')
+                [profileApiSimulation('123', '456'),mockProfileRespond],
+                [subscribeApiSimulation('789', '000'), mockSubscribeRespond],
             ])
-            .put(loginSuccess(
-                {type: loginAction.LOGIN_SUCCESS, payload: {response: 'test'}}
-            ))
-            .put(getSubscribeSuccess(
-                {type: subscribeAction.GET_SUBSCRIBE_SUCCESS, payload: {response: 'test'}}
-            ))
+            // .put(loginSuccess(
+            //     {type: loginAction.LOGIN_SUCCESS, payload: mockProfileRespond}
+            // ))
+            // .put(getSubscribeSuccess(
+            //     {type: subscribeAction.GET_SUBSCRIBE_SUCCESS, payload: mockSubscribeRespond}
+            // ))
             .run();
     });
 
